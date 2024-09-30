b36c53fcdc1df4694f65c94baa48b6e70ad9093b
rizqinurfadh <rizqinurfadhilah210@gmail.com>
2024-09-30 15:14:55 +0700

    include js

--- notareport/urls.py
+++ notareport/urls.py
@@ -3,6 +3,6 @@ from.import views
 
 urlpatterns = [
     path('dashboard-admin/', views.dashboard_admin, name='dashboard-admin'),
-    path('management-bbm/', views.report_admin_bbm, name='management-bbm')
+    path('report-bbm/', views.report_admin_bbm, name='report-bbm')
 ]
 
