7e488fdd8edc443bf234eb95a828c27c293ee90f
rizqinurfadh <rizqinurfadhilah210@gmail.com>
2024-10-01 19:58:11 +0700

    teknisi dahsboard

--- notareport/views.py
+++ notareport/views.py
@@ -6,3 +6,9 @@ def dashboard_admin(request):
 
 def report_admin_bbm(request):
     return render(request, 'finance/report/bbm.html')
+
+def dashboard_teknisi(request):
+    return render(request, 'teknisi/layouts/dashboard-teknisi.html')
+
+def histori(request):
+    return render(request, 'teknisi/layouts/dashboard-teknisi.html')
