df9f6cca277434d9c14c5415a6abf216fbfdcead
nelaa7 <nelaanjani22@gmail.com>
2024-10-01 14:57:16 +0700

    tampilin form kendaraan

--- notareport/urls.py
+++ notareport/urls.py
@@ -3,6 +3,8 @@ from.import views
 
 urlpatterns = [
     path('dashboard-admin/', views.dashboard_admin, name='dashboard-admin'),
-    path('report-bbm/', views.report_admin_bbm, name='report-bbm')
+    path('report-bbm/', views.report_admin_bbm, name='report-bbm'),
+    path('form-kendaraan/', views.form_kendaraan, name='form-kendaraan')
+
 ]
 
--- notareport/views.py
+++ notareport/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Kendaraan
 
 # View Finance
 def dashboard_admin(request):
@@ -6,3 +7,50 @@ def dashboard_admin(request):
 
 def report_admin_bbm(request):
     return render(request, 'finance/report/bbm.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def form_kendaraan(request):
+    form = Kendaraan()
+    nik = request.user.naker.nik if hasattr(request.user, 'naker') else ''
+    jenis_kbm_choices = Kendaraan.JENIS_KBM_CHOICES
+    context = {
+        'form': form,
+        'witel_choices': Kendaraan.WITEL_CHOICES,
+        'nik': nik,
+        'jenis_kbm_choices' : jenis_kbm_choices
+    }
+    return render(request, 'leader/kendaraan/form-kendaraan.html', context)
+
+
+# def form_kendaraan(request):
+#     witel_choices = Kendaraan.WITEL_CHOICES 
+#     nik = request.user.naker.nik if hasattr(request.user, 'naker') else ''
+
+
+#     return render(request, 'leader/kendaraan/form-kendaraan.html'),{
+#     'witel_choices': witel_choices
+
+        
+#     }
